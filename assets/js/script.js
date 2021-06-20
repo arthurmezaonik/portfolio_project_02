269eef3d140ffbd448c63018bb07d7f1aa92e6a4
arthurmezaonik <arthur.mezaonik@gmail.com>
2021-06-20 16:52:46 +0000

    Set up game functions

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+function runGame(){
+
+}
+
+function createDeck(){
+
+}
+
+function firstCards() {
+
+}
+
+function checkBusted() {
+
+}
+
+function hit(){
+
+}
+
+function stand(){
+
+}
+
+function countPoints(){
+
+}
+
+function comparePoints(){
+
+}
+
+function keepPlaying() {
+
+}
+
+function clearHands(){
+
+}
